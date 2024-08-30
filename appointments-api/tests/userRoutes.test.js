18bf80b3e9cc78875523382ee931b7a068eb82bc
joaolucas <joaolucascgbarbosa878@gmail.com>
2024-08-30 19:17:24 -0300

    feat: add test

---
+++ appointments-api/tests/userRoutes.test.js
@@ -0,0 +1,36 @@
+const request = require("supertest");
+const express = require("express");
+const sequelize = require("../config/database");
+const User = require("../models/User");
+const userRoutes = require("../routes/userRoutes");
+
+const app = express();
+app.use(express.json());
+app.use("/api", userRoutes);
+
+beforeAll(async () => {
+  await sequelize.sync({ force: true });
+});
+
+afterAll(async () => {
+  await sequelize.close();
+});
+
+describe("User Routes", () => {
+  it("should create a new user", async () => {
+    const res = await request(app).post("/api/users").send({
+      name: "Test User",
+      email: "test@example.com",
+      password: "password",
+    });
+
+    expect(res.statusCode).toEqual(201);
+    expect(res.body).toHaveProperty("id");
+  });
+
+  it("should retrieve all users", async () => {
+    const res = await request(app).get("/api/users");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.length).toBeGreaterThan(0);
+  });
+});
